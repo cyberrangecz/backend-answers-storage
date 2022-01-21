a3564b4679586233a88b0771c1e116b9d1d1892b
Dominik Pilár <445537@mail.muni.cz>
2022-01-21 10:33:52 +0100

    Revert changes made in the migration sql script.

--- src/main/resources/db/migration/postgresql/V1__db_kypo_answers_storage_schema.sql
+++ src/main/resources/db/migration/postgresql/V1__db_kypo_answers_storage_schema.sql
@@ -1,16 +1,16 @@
 CREATE TABLE sandbox_info (
-                              sandbox_info_id bigserial NOT NULL PRIMARY KEY,
-                              sandbox_ref_id  int8      NOT NULL UNIQUE
+    sandbox_info_id bigserial NOT NULL PRIMARY KEY,
+    sandbox_ref_id  int8      NOT NULL UNIQUE
 );
 ALTER SEQUENCE sandbox_info_sandbox_info_id_seq RENAME TO sandbox_info_id_seq;
 ALTER SEQUENCE sandbox_info_id_seq INCREMENT 50;
 
 CREATE TABLE sandbox_answer (
-                                sandbox_answer_id bigserial    NOT NULL PRIMARY KEY,
-                                answer_content    varchar(255) NOT NULL,
-                                answer_variable_name varchar(255) NOT NULL,
-                                sandbox_info_id   int8,
-                                FOREIGN KEY (sandbox_info_id) REFERENCES sandbox_info
+    sandbox_answer_id bigserial    NOT NULL PRIMARY KEY,
+    answer_content    varchar(255) NOT NULL,
+    answer_variable_name varchar(255) NOT NULL,
+    sandbox_info_id   int8,
+    FOREIGN KEY (sandbox_info_id) REFERENCES sandbox_info
 );
 ALTER SEQUENCE sandbox_answer_sandbox_answer_id_seq RENAME TO sandbox_answer_id_seq;
 ALTER SEQUENCE sandbox_answer_id_seq INCREMENT 50;
